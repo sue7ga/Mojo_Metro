8ccdf38b081c64066fe7acd2a7846d640cbdfe22
sue7ga <sue77ga@gmail.com>
2014-09-29 11:55:28 -0700

    ajax added

--- metro.pl
+++ metro.pl
@@ -8,20 +8,21 @@ my $metro = Metro->new(api_key => 'e4346dc05e12b8e457bdfe693a858f83aa7a31ebed6af
 
 get '/' => sub{
  my $self = shift;
+ my $line = $metro->line_japanese;
+ print Dumper $line;
  $self->render('index');
 };
 
 get '/foo.json' => sub{
  my $self = shift;
  my $line = $metro->line_japanese;
- my $data = JSON::encode_json($line); 
- print Dumper $data;
- $self->render(json => $data);
+ $self->render(json => $line);
 };
 
 app->start;
 
 __DATA__
+
 @@ index.html.ep
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
 "http://www.w3.org/TR/html4/loose.dtd">
@@ -34,20 +35,28 @@ __DATA__
 <body>
 <ul id="output"></ul>
 <script type="text/javascript">
-function showMsg(){
-$.ajax({
- type:'GET',
- url:'http://localhost:3000/foo.json',
- dataType:'json',
-success: function(json){
-  for(var i in json){
-     alert(json[i].linename);
+  $(document).ready(function(){
+  $.ajax({
+   type:'GET',
+   url:'http://localhost:3000/foo.json',
+   dataType:'json',
+   success: function(json){
+    for(var i in json){
+     $("#output").append("<li><strong>" + json[i].linename + "</li></strong>");
+      for(var j in json[i].line){
+            $("#output").append("<li>&nbsp;" + json[i].line[j].japanese + "(" + json[i].line[j].line +")" + "</li>");
+      }
+    }
   }
- }
+ });
 });
-}
 </script>
-<button onClick="showMsg();">Click</button>
+<style type="text/css">
+ #output{
+   font-size: 10px;
+   background: #cc9999;
+ }
+</style>
 </body>
 </html>
 
